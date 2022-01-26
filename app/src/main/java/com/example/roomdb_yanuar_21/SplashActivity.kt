57e5a75ca1e8d8323023be2a9db89fed9ebf7486
PradanaYP <yanuartampan5gmail@.com>
2022-01-26 11:49:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/roomdb_yanuar_21/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.example.roomdb_yanuar_21
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+
+    private val Splasscreentimeout : Long = 2000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, Splasscreentimeout)
+
+
+        }
+
+    }
+
